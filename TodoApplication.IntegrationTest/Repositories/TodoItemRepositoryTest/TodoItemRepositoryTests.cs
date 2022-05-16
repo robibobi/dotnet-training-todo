797bb4ce689afb12ac6893daab962cbdbb0acc6b
Tobias Hoefer <Tobias.Hoefer@krones.com>
2022-05-16 09:54:31 +0200

    Refactored integration tests for the TotoItemRepository type.

--- TodoApplication.IntegrationTest/Repositories/TodoItemRepositoryTest/TodoItemRepositoryTests.cs
+++ TodoApplication.IntegrationTest/Repositories/TodoItemRepositoryTest/TodoItemRepositoryTests.cs
@@ -4,9 +4,7 @@ using Shouldly;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TodoApplication.Models;
 using TodoApplication.Respositories;
 using TodoApplication.Services;
 
@@ -16,46 +14,101 @@ namespace TodoApplication.IntegrationTest.Repositories.TodoItemRespositoryTest
     [TestClass]
     public class TodoItemRepositoryTests
     {
+        private readonly string _testDir = Path.Combine(Path.GetTempPath(), "TodoItemRepositoryTests");
+
         [TestMethod]
         public void GetAll_FileContainsTwoItems_ReturnsListWithTwoItems()
         {
             // Arrange
-            // Create a unqiue directory for this thest method
-            var testDir = Path.Combine(Path.GetTempPath(), "TodoAppTest", Path.GetRandomFileName());
-            try
-            {
-                if (Directory.Exists(testDir))
-                {
-                    Directory.Delete(testDir, true);
-                }
-                Directory.CreateDirectory(testDir);
-                // Copy test files to directory
-                var sourceFileName = Path.Combine(Environment.CurrentDirectory, "Repositories", "TodoItemRepositoryTest", "Data", "GetAllTestFile.json");
-                var targetFileName = Path.Combine(testDir, "GetAllTestFile.json");
-                File.Copy(sourceFileName, targetFileName, true);
-
-
-                var todoItemTestFile = new FileInfo(targetFileName);
-                var appConfigMock = new Mock<IAppConfigService>();
-                appConfigMock.Setup(s => s.TodoItemFile).Returns(todoItemTestFile);
-
-                var repository = CreateSut(appConfigMock.Object);
-                // Act
-                var todos = repository.GetAll();
-                // Assert
-                todos.Count.ShouldBe(2);
-                // Cleanup
-
-            }
-            finally
-            {
-                // Remove the created test directory
-                Directory.Delete(testDir, true);
-            }
+            RecreateDirectory(_testDir);
+            var todoItemTestFile = CopyFileToTestDir(_testDir, "GetAllTestFile.json");
+            var repository = CreateSut(todoItemTestFile);
+            // Act
+            var todos = repository.GetAll();
+            // Assert
+            todos.Count.ShouldBe(2);
         }
 
-        private ITodoItemRepository CreateSut(IAppConfigService configService)
+        [TestMethod]
+        public void Add_FileIsEmpty_ItemIsAddedToTheFile()
         {
+            // Arrange
+            RecreateDirectory(_testDir);
+            var todoItemTestFile = CopyFileToTestDir(_testDir, "EmptyTestFile.json");
+            var repository = CreateSut(todoItemTestFile);
+            // Act
+            repository.Add(CreateTodoItem("Create integration tests"));
+
+            // Assert
+            File.ReadAllText(todoItemTestFile.FullName).ShouldContain("\"Name\": \"Create integration tests\"");
+        }
+
+        [TestMethod]
+        public void Remove_FileHas2TodoItems_FirstItemIsRemovedFromFile()
+        {
+            // Arrange
+            RecreateDirectory(_testDir);
+            var testFile = CopyFileToTestDir(_testDir, "GetAllTestFile.json");
+            var repository = CreateSut(testFile);
+            // Act
+            repository.Remove(Guid.Parse("6cd21072-1260-45d3-a4ec-60859de7a12b"));
+
+            // Assert
+            File.ReadAllText(testFile.FullName).ShouldNotContain("6cd21072-1260-45d3-a4ec-60859de7a12b");
+        }
+
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            // Cleanup
+            Directory.Delete(_testDir, true);
+        }
+
+
+        private FileInfo CopyFileToTestDir(string testDirPath, string fileName)
+        {
+            var sourceFileName = Path.Combine(Environment.CurrentDirectory, "Repositories", "TodoItemRepositoryTest", "Data", fileName);
+            var targetFileName = Path.Combine(testDirPath, fileName);
+            File.Copy(sourceFileName, targetFileName, true);
+            return new FileInfo(targetFileName);
+        }
+
+        private void RecreateDirectory(string directory)
+        {
+            if (Directory.Exists(directory))
+            {
+                Directory.Delete(directory, true);
+            }
+            Directory.CreateDirectory(directory);
+        }
+
+        private IAppConfigService CreateFakeConfigService(FileInfo todoItemFile)
+        {
+            var appConfigMock = new Mock<IAppConfigService>();
+            appConfigMock.Setup(s => s.TodoItemFile).Returns(todoItemFile);
+
+            return appConfigMock.Object;
+        }
+
+
+        private TodoItem CreateTodoItem(string name)
+        {
+            return new TodoItem()
+            {
+                Name = name,
+                Id = Guid.NewGuid(),
+                IsDone = false,
+                TagIds = new List<Guid>(),
+                TimeStamp = DateTime.Now,
+            };
+        }
+
+
+        private ITodoItemRepository CreateSut(FileInfo testFile)
+        {
+            var configService = CreateFakeConfigService(testFile);
+
             return new TodoItemRespository(configService);
         }
     }
